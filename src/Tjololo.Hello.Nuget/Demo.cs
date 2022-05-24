d222021f3e472fb11f740adda3cde783db9a515c
Vemund Gaukstad <tjololo@users.noreply.github.com>
2022-05-24 14:19:27 +0200

    add GetSmallestNumber method

--- src/Tjololo.Hello.Nuget/Demo.cs
+++ src/Tjololo.Hello.Nuget/Demo.cs
@@ -19,4 +19,21 @@ public class Demo
         return largestNumber;
     }
 
+    /// <summary>
+    /// GetSmallestNumber - Returns the smallest number from the given list of numbers.
+    /// </summary>
+    /// <param name="numbers">The list of numbers.</param>
+    /// <returns>The smallest number.</returns>
+    public int GetSmallestNumber(int[] numbers)
+    {
+        int smallestNumber = numbers[0];
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (numbers[i] < smallestNumber)
+            {
+                smallestNumber = numbers[i];
+            }
+        }
+        return smallestNumber;
+    }
 }
