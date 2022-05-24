760d8943fa79e99a10b2faa0ba5c87cc3f15ee40
Vemund Gaukstad <tjololo@users.noreply.github.com>
2022-05-24 11:32:23 +0200

    simple method

---
+++ src/Tjololo.Hello.Nuget/Demo.cs
@@ -0,0 +1,22 @@
+﻿namespace Tjololo.Hello.Nuget;
+public class Demo
+{
+    /// <summary>
+    /// GetLargestNumber - Returns the largest number from the given list of numbers.
+    /// </summary>
+    /// <param name="numbers">The list of numbers.</param>
+    /// <returns>The largest number.</returns>
+    public int GetLargestNumber(int[] numbers)
+    {
+        int largestNumber = numbers[0];
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (numbers[i] > largestNumber)
+            {
+                largestNumber = numbers[i];
+            }
+        }
+        return largestNumber;
+    }
+
+}
